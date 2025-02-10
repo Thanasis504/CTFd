89efab3a3178d663ea1524fd44a19e2bac743fb7
Thanasis504 <akshay.kollara@gmail.com>
2025-02-11 03:33:19 +0530

    Updated db.go to use Render DATABASE_URL

--- api/internal/db/db.go
+++ api/internal/db/db.go
@@ -2,27 +2,33 @@ package database
 
 import (
 	"log"
+	"os"
 
 	"github.com/ctf-api/internal/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-// InitDB initializes and returns a GORM database instance
-func InitDB(config ) *gorm.DB {
-	dsn := "host=localhost user=postgres password=partner dbname=ctf_db port=5432 sslmode=disable"
+func InitDB() *gorm.DB {
+	// Get DATABASE_URL from environment variables
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("❌ DATABASE_URL is not set")
+	}
+
+	// Connect to PostgreSQL
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("❌ Failed to connect to database: %v", err)
 	}
 
-	// AutoMigrate will create the teams table if it doesn’t exist
+	// AutoMigrate will create tables if they don’t exist
 	err = db.AutoMigrate(
 		&models.Team{},
-	    &models.Challenge{},
+		&models.Challenge{},
 		&models.Submission{},
 		&models.Score{},
-       )
+	)
 	if err != nil {
 		log.Fatalf("❌ AutoMigration failed: %v", err)
 	}
